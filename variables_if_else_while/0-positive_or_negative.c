e2a70ca322fb4a68ac8b4b777c8a47dcaf52df43
Trey <trey.turner1857@gmail.com>
2024-01-19 12:12:30 -0600

    im confused

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - good file
+ * Return: 0 (always)
+ */
+
+int main(void)
+{
+	int n ;
+	if (n > 0) {
+		printf("is positive");
+	} else if (n == 0) {
+		printf("is zero");
+	} else (n < 0) {
+		printf("is negative");
+	}
+
+	return (0);
+}	
+
+
+
+
