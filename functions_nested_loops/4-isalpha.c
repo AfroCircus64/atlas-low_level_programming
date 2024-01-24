3d204281b950bdb15db838eccfd1f266b04dfcac
Trey <trey.turner1857@gmail.com>
2024-01-23 19:32:13 -0600

    first go at this

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - checks for a lower case and upper case letter
+ *@c: parameter
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
