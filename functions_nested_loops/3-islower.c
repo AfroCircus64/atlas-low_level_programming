396f6108b28aa40e8e2cabc0d3cb55125a62a386
Trey <trey.turner1857@gmail.com>
2024-01-23 19:22:26 -0600

    i think i did this right

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - checks for a lower case letter
+ *
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
