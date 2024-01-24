bd61d0d9eaea952a01e1268e484f5b75c62b17d9
Trey <trey.turner1857@gmail.com>
2024-01-23 18:55:18 -0600

    i really need these points

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
- * main - main function
+ * main - prints _putchar
  * Return: Always 0
  */
 
-int _putchar(char c)
+int main(void)
 {
-	char c;
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,5 +15,6 @@ int _putchar(char c)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
