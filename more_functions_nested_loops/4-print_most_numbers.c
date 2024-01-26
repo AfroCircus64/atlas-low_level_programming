0a0bea2e526f737357b8f42b06b75f47827e96ee
Trey <trey.turner1857@gmail.com>
2024-01-26 00:06:18 -0600

    first attempt

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints 0 - 9 except 2 and 4
+ *
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int m;
+
+	for (m = 0; m <= 9; m++)
+	{
+		if (m != 2 && m != 4)
+		{
+			_putchar(i + '0');
+		}
+		_putchar("\n")
+	}
+}
