aca09460b06e3c454fcf338af143e686ba35cbb6
Trey <trey.turner1857@gmail.com>
2024-01-26 10:39:23 -0600

    print a line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ *
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	n = 10;
+
+	if (n > 0)
+	{
+		_putchar('_');
+	}
+	else 
+	{
+		_putchar('\n');
+	}
+}
+
