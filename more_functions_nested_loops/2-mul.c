95821716cf0402ad43d0b3e94afe3832dab3abcc
Trey <trey.turner1857@gmail.com>
2024-01-25 23:13:17 -0600

    first try

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * mul - multiplies two numbers
+ * @a: integer to be multiplied
+ * @b: integer to be multiplied
+ * Return: Always 0
+ */
+
+int mul(int a, int b);
+{
+	printf("%d\n", mul(10, 28);
+	return (0);
+}
