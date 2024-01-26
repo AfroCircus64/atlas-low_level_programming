273b86abf4d1a78eac189dae683e14c958a9929a
Trey <trey.turner1857@gmail.com>
2024-01-25 23:26:26 -0600

    rewrote the code after getting some assistance

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,14 +3,10 @@
  * mul - multiplies two numbers
  * @a: integer to be multiplied
  * @b: integer to be multiplied
- * Return: Always 0
+ * Return: result of the multiplication
  */
 
 int mul(int a, int b)
 {
-	for(a = 10; b = 19)
-	{
-		printf("%d\n", mul);
-	}
-	return (0);
+	return (a * b);
 }
