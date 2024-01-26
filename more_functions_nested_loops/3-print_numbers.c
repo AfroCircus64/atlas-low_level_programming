fad98f64aa41147bcaab3f1157fbc4115c285971
Trey <trey.turner1857@gmail.com>
2024-01-25 23:33:25 -0600

    first try

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0 - 9 followed by a new line
+ *
+ * Return: 0 Always
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+}
