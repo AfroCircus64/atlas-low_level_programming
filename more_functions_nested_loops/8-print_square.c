73f566e36a2748c4e199a454cb5ff958b39105e1
Trey <trey.turner1857@gmail.com>
2024-01-28 21:01:47 -0600

    python tutor is super helpful

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_square - prints squares using #
+ * @size: size of the square
+ * Return: Always 0
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n')
+	}
+	else
+	{
+		int i;
+
+		int k;
+
+		for (i = 0; i < size; i++)
+		{
+			for (k = 0; k < size; k++)
+			{
+				_putchar('#');
+			}
+			_puthcar('\n');
+		}
+	}
+}
