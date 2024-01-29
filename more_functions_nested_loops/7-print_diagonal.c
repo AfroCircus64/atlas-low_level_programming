706ce96173abc8b61098c5f7676b507cb9210af9
Trey <trey.turner1857@gmail.com>
2024-01-28 20:22:24 -0600

    after an hour at python tutor i think i got it

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line in the terminal
+ * @n: parameter
+ * Return: Always 0
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		int k;
+
+		for (i = 0; i < n; i++)
+		{
+			for (k = 0; k <= i; k++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
