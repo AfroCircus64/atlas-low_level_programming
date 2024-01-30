4cb835354c0107a91609b992e008bc34bf1e32d9
Trey <trey.turner1857@gmail.com>
2024-01-29 19:35:16 -0600

    first attempt

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of a int to 98
+ * 
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	int i = 5;
+	int *n = &i;
+
+	printf("n = %d\n", *n);
+
+	*n = 98;
+
+	printf("n = %d\n", *n);
+}
