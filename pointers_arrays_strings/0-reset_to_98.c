2d5001628602a0653163d781576f95cf789a3862
Trey <trey.turner1857@gmail.com>
2024-01-29 19:56:47 -0600

    checking if im overthinking this

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,17 +2,11 @@
 #include <stdio.h>
 /**
  * reset_to_98 - updates the value of a int to 98
- * 
+ * @n: parameter
  * Return: Always 0
  */
 
 void reset_to_98(int *n)
 {
-	int n = 5;
-
-	printf("n = %d\n", *n);
-
-	reset_to_98(&n);
-
-	printf("n = %d\n", *n);
+	*n = 98;
 }
