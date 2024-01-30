2e0cc68ebb0220794613e569b107eb6f1b26a0ea
Trey <trey.turner1857@gmail.com>
2024-01-29 21:32:24 -0600

    i fear i am underthinking this one. it cant be this easy, right?????

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: parameter
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	printf("%d\n", str);
+}
