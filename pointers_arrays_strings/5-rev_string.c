17af2f4e7661e2cd9778aed785c0de50ac0ea654
Trey <trey.turner1857@gmail.com>
2024-01-29 22:55:07 -0600

    not sure about this one

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - reverses a string
+ * @s: parameter
+ * Return: Always 0
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int length = 0;
+	char c;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	int mid = length;
+
+	for (i = 0; i < mid; i++)
+	{
+		c = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = c;
+	}
+}
