3f90ef70b56388669f53f23f2df7298469b6711a
Trey <trey.turner1857@gmail.com>
2024-02-19 23:24:30 -0600

    i really need to stop waiting until last minute

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _calloc - function that allocates memory for an array
+ * @nmemb: parameter
+ * @size: parameter
+ * Return: Always 0
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0;
+	int k = 0;
+	char *c;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	k = nmemb * size;
+	c = malloc(k);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i < k)
+	{
+		c[i] = 0;
+		i++;
+	}
+
+	return (c);
+}
