1a8f1fe4cfec820880f63999abbc1a8cb00bd57f
Trey <trey.turner1857@gmail.com>
2024-02-25 17:24:22 -0600

    first commit

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ * Return: Always 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
