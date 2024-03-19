ec0a2598c3cdc4834cf0bfba0bca1135b7932249
Trey <trey.turner1857@gmail.com>
2024-03-18 21:21:55 -0500

    worked through the failing checks

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,6 +11,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
+	dlistint_t *temp;
 
 	new_node = malloc(sizeof(dlistint_t));
 
@@ -20,7 +21,24 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new_node->n = n;
-	new_node->next = *head;
+	new_node->prev = NULL;
+	temp = *head;
+
+	if (temp != NULL)
+	{
+		while (temp->prev != NULL)
+		{
+			temp = temp->prev;
+		}
+	}
+
+	new_node->next = temp;
+
+	if (temp != NULL)
+	{
+		temp->prev = new_node;
+	}
+
 	*head = new_node;
 
 	return (new_node);
