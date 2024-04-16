5c81d9c601946584b2816f896c94da6663f6d9de
Trey Turner <Trey.turner1857@gmail.com>
2024-04-16 10:15:43 -0500

    created the 1-binary.c file and replaced 0-main.c with 1-main.c

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * @array: sorted array of ints
+ * @size: size of sorted array
+ * @value: value to find
+ * Return: value if success or -1 if fail
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+
+}
