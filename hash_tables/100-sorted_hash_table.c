de55c20eeca1a9cf0582b67e0462f07426f70ea9
Trey Turner <trey.turner1857@gmail.com>
2025-02-06 10:06:11 -0600

    Created the main file and added function definitions in the .c

--- hash_tables/100-sorted_hash_table.c
+++ hash_tables/100-sorted_hash_table.c
@@ -1,1 +1,76 @@
-/* Code will go here */
+#include "hash_tables.h"
+
+
+/**
+ * shash_table_create - Creates a hash table
+ *
+ * @size: Size of the table
+ *
+ * Return: 0 (success)
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	/* Code */
+}
+
+
+/**
+ * shash_table_set - Add an element to the table
+ *
+ * @ht: Hash table
+ * @key: Key
+ * @value: Value
+ *
+ * Return: 0 (success)
+ */
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+	/* Code */
+}
+
+
+/**
+ * shash_table_get - Gets a specific element in a table
+ *
+ * @ht: Hash table
+ * @key: Key
+ *
+ * Return: 0 (success)
+ */
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	/* Code */
+}
+
+
+/**
+ * shash_table_print - Prints the table
+ *
+ * @ht: Hash table
+ */
+void shash_table_print(const shash_table_t *ht)
+{
+	/* Code */
+}
+
+
+/**
+ * shash_table_print_rev - Prints the table in reverse
+ *
+ * @ht: Hash table
+ */
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	/* Code */
+}
+
+
+/**
+ * shash_table_delete - Deletes a table
+ *
+ * @ht: hash table
+ */
+void shash_table_delete(shash_table_t *ht)
+{
+	/* Code */
+}
