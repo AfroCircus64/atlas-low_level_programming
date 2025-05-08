0b2ff2eaa680b1e8b2ba0a6314563e85b83f0b8a
Trey Turner <trey.turner1857@gmail.com>
2025-05-08 11:32:44 -0500

    Initial commit. Added necessary files.

---
+++ search_algorithms/102-interpolation.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - Searches for a value in a sorted array of integers
+ *
+ * @array: A pointer to the first element of the array to search in.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: The index where value is located, or -1 if not found.
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+    /* Code goes here */
+}
