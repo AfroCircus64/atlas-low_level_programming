bb4eb05846f0bf0fb7a34956f4c13ca33bc417b8
Trey Turner <trey.turner1857@gmail.com>
2025-05-13 11:47:29 -0500

    Pushing what I have for task 0 (Not fully working. Will continue after work)

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -11,5 +11,42 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-    /* Code goes here */
+	size_t low, high, pos;
+
+	low = 0;
+	high = size - 1;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+
+		if (pos > (unsigned int)size)
+		{
+			printf("Value checked array[%ld] is out of range\n", pos);
+			return (-1);
+		}
+
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+
+		if (array[pos] == value)
+		{
+			return (pos);
+		}
+
+		if (array[pos] < value)
+		{
+			low = pos + 1;
+		}
+		else
+		{
+			high = pos - 1;
+		}
+	}
+
+	return (-1);
 }
