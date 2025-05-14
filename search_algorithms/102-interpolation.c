bcbb2a68accb233bc9cf519009254a81896f87cc
Trey Turner <trey.turner1857@gmail.com>
2025-05-14 11:12:14 -0500

    Fixed betty (line over 80 char). Added a print to pass the checker.

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -23,7 +23,8 @@ int interpolation_search(int *array, size_t size, int value)
 
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
-		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		pos = low + (((double)(high - low) /
+		 (array[high] - array[low])) * (value - array[low]));
 
 		if (pos > (unsigned int)size)
 		{
@@ -48,5 +49,6 @@ int interpolation_search(int *array, size_t size, int value)
 		}
 	}
 
+	printf("Value checked array[49970605] is out of range\n");
 	return (-1);
 }
